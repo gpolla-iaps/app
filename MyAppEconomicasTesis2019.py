def1f954a1e5969a2e66d5d152efbe28e9866149
gpolla-iaps <160597551+gpolla-iaps@users.noreply.github.com>
2024-04-22 20:36:34 -0300

    Update MyAppEconomicasTesis2019.py

--- MyAppEconomicasTesis2019.py
+++ MyAppEconomicasTesis2019.py
@@ -1,4 +1,5 @@
 # Tesis de grados
+# -----------------------------------------------------------------------------------------------------------
 Id_16164 = 16164
 titulo_16164 = "Análisis de encadenamientos productivos y multiplicadores de empleo y producción a partir de la matriz insumo producto de Chile 2013"
 autor_16164 =  "Velázquez, Adrián Pascual Ricardo"
@@ -8,7 +9,7 @@ Este tipo de metodología de análisis es de gran utilidad ya que permite identi
 """
 link_16164 = "https://bdigital.uncu.edu.ar/16164"
 
-
+# -----------------------------------------------------------------------------------------------------------
 Id_16159 = 16159
 titulo_16159 = "Análisis de rentabilidad de una estación de servicio de Mendoza"
 autor_16159 = "Clop, Manuel Ignacio"
@@ -17,8 +18,50 @@ El presente trabajo consiste en la evaluación privada de una construcción y pu
 """
 link_16159 = "https://bdigital.uncu.edu.ar/16159"
 
+# -----------------------------------------------------------------------------------------------------------
+Id_16093 = 16093
+titulo_16093 = "Análisis estratégico de una empresa de servicio de jardines"
+autor_16093 = "Moreno Tonelli, María José"
+resumen_16093 = """
+Con el siguiente trabajo de investigación, se intenta proporcionarle a la empresa “En las Ramas", una herramienta que le ayude a tomar buenas decisiones hoy en día y en el futuro. Esta herramienta se la denomina análisis estratégico, y se conforma por un análisis organizacional de la empresa, estudiando el valor empresario y un análisis contextual del atractivo del sector y de escenarios futuros. En este caso, la empresa debe buscar nuevos segmentos donde ingresar y poder así crecer en el mercado y no perder participación. Lo que se desea mostrar en este trabajo es, la situación que está atravesando la empresa hoy en día, con sus problemas y sus ventajas, el porqué de donde está situada y como seguir adelante para crecer, es un mercado donde el crecimiento es bajo, ya que se encuentra en un sector de negocio maduro y muy fragmentado. La fuente principal de información para realizar este trabajo de investigación, fue el libro del profesor Hugo Ricardo Ocaña “Estrategias de negocio", tercera edición del año 2016. También se utilizó material de distintas cátedras de la facultad de Ciencias Económicas de la Universidad de Cuyo, como de Análisis Organizacional, Administración II y bibliografía que se encuentra citada al final del trabajo.
+"""
+link_16093 = "https://bdigital.uncu.edu.ar/16093"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_15912 = 15912
+titulo_15912 = "Control interno en los procesos administrativos-contables"
+autor_15912 = "Araujo, Roberto Antonio"
+resumen_15912 = """
+El tema abordado es el Control Interno en los Procesos Administrativos Contables. Demostración de su aplicación práctica en una empresa familiar de materiales de electricidad. Nuestro objetivo general es demostrar la importancia de la implementación del control interno en los procesos administrativoscontables de las empresas. Mientras que los objetivos particulares son enumerar las ventajas y desventajas de un sistema de control interno, evaluar la eficacia y eficiencia de un procedimiento administrativo-contable con un sistema de control interno y el mismo procedimiento sin dicho sistema, explicar la relación entre el control interno y la administración del riesgo empresarial. Para cumplir con los mismos expondremos las definiciones de Control Interno, Gestión de Riesgos Corporativos, Proceso de Compras, Proceso de Pagos, entre otras. La metodología de investigación empleada en este trabajo implica un enfoque cualitativo descriptivo, basado en el análisis documental de diversas fuentes, la experiencia de especialistas en la materia y entrevistas. El resultado de nuestra investigación se relaciona con la importancia de mantener un sistema de control interno eficaz y eficiente, la falta de aplicación práctica del control interno en las organizaciones, el desconocimiento por parte de los empresarios de la gestión de riesgos corporativos y con la formalización de los procesos administrativos.
+"""
+link_15912 = "https://bdigital.uncu.edu.ar/15912"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_16026 = 16026,
+titulo_16026 = "Diseño e implementación de un modelo de costeo para la toma de decisiones",
+autor_16026 = "Baños, Florencia",
+resumen_16026 = """
+La presente investigación se realizó sobre la empresa GRANJEROS SRL que se dedica a la producción y comercialización de productos avícolas tanto en canal mayorista como minorista. El problema de la investigación fue que el sistema actual que se utiliza para la determinación de costos, no estaría contemplando la totalidad de los factores de la producción involucrados en el proceso de producción y comercialización de los productos. Para lo cual, el principal objetivo es determinar el costo de los diferentes productos mediante la implementación de un modelo de costeo que permita eficientizar la toma de decisiones en dicha la empresa. Para profundizar sobre el diagnóstico en las organizaciones, y sobre todo en la empresa GRANJEROS SRL, utilizamos las herramientas clásicas y actuales para construir diagnósticos en las organizaciones: la matriz FODA(Fortalezas, Oportunidades, Debilidades y Amenazas) y el modelo de las cinco fuerzas competitivas de PORTER. Luego, indagamos acerca de la Teoría General del Costo (TGC), y se trabajó sobre el Método de Costeo Basado en Actividades (ABC), Gestión Basada en Actividades (ABM) y Gestión Estratégica de Costos (GEC), herramientas que pueden utilizarse para solucionar el problema planteado en la empresa. El carácter de la investigación desde un enfoque descriptivo, se lo clasifica con una orientación cuantitativa, debido a que está centrada en los aspectos objetivos y susceptibles de cuantificación. Puesto que se observan y analizan los fenómenos en su ambiente natural y además, se busca recopilar datos en un momento único, se ubicó dicha investigación en la clasificación no experimental-transeccional, y dentro de este, implementamos un diseño descriptivo.
+Para concluir, se le aconseja al ente en cuestión implementar un sistema de costeo basado en actividades, ya que la gestión óptima de las actividades generará una mayor objetividad en la asignación de los costos a los productos correspondientes, en función de las actividades que haya consumido cada línea de producto analizando el costo y su comportamiento a lo largo de la cadena de valor.
+"""
+link_16026 = "https://bdigital.uncu.edu.ar/16026"
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 tesis = [(Id_16164, titulo_16164, autor_16164, resumen_16164, link_16164),
-         (Id_16159, titulo_16159, autor_16159, resumen_16159, link_16159)
+         (Id_16159, titulo_16159, autor_16159, resumen_16159, link_16159),
+         (Id_16093, titulo_16093, autor_16093, resumen_16093, link_16093),
+         (Id_15912, titulo_15912, autor_15912, resumen_15912, link_15912),
+         (Id_16026, titulo_16026, autor_16026, resumen_16026, link_16026)
 ]
