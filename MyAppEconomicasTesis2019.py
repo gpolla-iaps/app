26158cec571551fd68bf7acd8919a8bdaf5321a7
gpolla-iaps <160597551+gpolla-iaps@users.noreply.github.com>
2024-04-22 19:56:19 -0300

    Add files via upload

---
+++ MyAppEconomicasTesis2019.py
@@ -0,0 +1,24 @@
+# Tesis de grados
+Id_16164 = 16164
+titulo_16164 = "Análisis de encadenamientos productivos y multiplicadores de empleo y producción a partir de la matriz insumo producto de Chile 2013"
+autor_16164 =  "Velázquez, Adrián Pascual Ricardo"
+resumen_16164 =""" 
+A partir del modelo Insumo Producto se puede estudiar el efecto sobre el total de la economía de shocks exógenos en la demanda final de bienes de un determinado sector. Para ello se recurre al cálculo de “multiplicadores" que reflejan la amplificación del shock inicial en un sector sobre el resto de la economía a través del flujo de comercio intersectorial. Otro análisis que suele hacerse es el estudio de los llamados “encadenamientos" productivos, por medio del cual se busca identificar la interdependencia que presentan los diferentes sectores, debido a sus requerimientos de insumos.
+Este tipo de metodología de análisis es de gran utilidad ya que permite identificar los sectores productivos hacia los cuales podrían dirigirse políticas públicas de apoyo y estímulo, para promover un mayor crecimiento en la actividad económica y en la generación de puestos de trabajo. El trabajo de investigación consiste en un análisis cuantitativo, que abarca el cálculo y análisis de indicadores (encadenamientos y multiplicadores de producción y empleo) a partir de los datos disponibles de la matriz insumo producto de Chile del año 2013, con el objetivo de determinar cuáles son los sectores económicos de este país que estimulan un mayor crecimiento en la producción de otros sectores y en la generación de empleo en su economía. Del análisis de los indicadores se concluye que los sectores que promueven un mayor incremento de producción en la economía son el Agropecuario-silvícola y pesca; la Industria manufacturera; el sector de Electricidad, gas, agua y gestión de desechos; el de Transporte, comunicaciones y servicios de información; la Construcción; y el sector de Comercio, hoteles y restaurantes. De éstos, los dos últimos también estimulan una mayor generación de empleo de mano de obra, y los cuatro primeros son sectores clave por sus encadenamientos tanto hacia atrás como hacia adelante.
+"""
+link_16164 = "https://bdigital.uncu.edu.ar/16164"
+
+
+Id_16159 = 16159
+titulo_16159 = "Análisis de rentabilidad de una estación de servicio de Mendoza"
+autor_16159 = "Clop, Manuel Ignacio"
+resumen_16159 = """
+El presente trabajo consiste en la evaluación privada de una construcción y puesta en marcha de una estación de servicio en Mendoza, Argentina en el año 2020. Dicha investigación, tiene como objetivo general analizar si el proyecto de la construcción de una estación de servicio es rentable, del cual se desprenden 2 objetivos específicos, los cuales son: Calcular y analizar los indicadores: Valor actual neto, tasa interna de retorno y periodo de recupero de inversión. Y el otro objetivo especifico es, describir y cuantificar los escenarios pesimista, moderado pesimista, moderado optimista y optimista y analizar si son o no rentables. Se puede apreciar en los objetivos, que la investigación tiene un enfoque cuantitativo marcado. Para lograr el objetivo general se creó un flujo de beneficios y costos, previamente valuados y cuantificados, con el fin de obtener los indicadores de rentabilidad. Donde el VAN es positivo, y la tasa interna de retorno es ampliamente superior al costo de oportunidad, por lo que conviene ejecutar el proyecto. Mientras que el periodo de recupero es de 7 años. Por último, se considera el riesgo en la investigación, donde se aplican métodos para la incorporación del mismo. Ellos, son la determinación de variables críticas, que son aquellas que cuya variabilidad influyen mas fuertemente en la variabilidad de la rentabilidad del proyecto, análisis de sensibilidad, punto de nivelación y análisis de escenarios. Este ultimo método, esta íntegramente relacionado con el segundo objetivo especifico donde se realizan combinaciones alternativas de los valores de las variables mas relevantes tales como, tasa de descuento, inversión inicial y venta de combustible con el fin de desarrollar un rango para el indicador de rentabilidad (VAN). Se pudo mostrar que, para 5 escenarios, el único que tuvo VAN negativo, es decir, que no es rentable, es en el mas pesimista, mientras que los demás, eran todos rentables. Además, se realizo un Valor Actual Neto Esperado, donde a cada escenario se le otorgaba una probabilidad de ocurrencia, donde el cual resulto ser positivo, por lo tanto, muestra la conveniencia de la ejecución del proyecto.
+"""
+link_16159 = "https://bdigital.uncu.edu.ar/16159"
+
+
+
+tesis = [(Id_16164, titulo_16164, autor_16164, resumen_16164, link_16164),
+         (Id_16159, titulo_16159, autor_16159, resumen_16159, link_16159)
+]
