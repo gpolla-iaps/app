c3259ceba49ecbb968ac75f42f993164f92e173b
gpolla-iaps <160597551+gpolla-iaps@users.noreply.github.com>
2024-04-22 21:25:36 -0300

    Update MyAppEconomicasTesis2019.py

--- MyAppEconomicasTesis2019.py
+++ MyAppEconomicasTesis2019.py
@@ -46,6 +46,93 @@ Para concluir, se le aconseja al ente en cuestión implementar un sistema de cos
 """
 link_16026 = "https://bdigital.uncu.edu.ar/16026"
 
+# -----------------------------------------------------------------------------------------------------------
+Id_16018 = 16018
+titulo_16018 = "Economía del comportamiento aplicada a las telecomunicaciones"
+autor_16018 = "Legaspe, Francisco"
+resumen_16018 = """
+A nivel mundial, el mercado de servicios de telefonía móvil es un mercado relativamente nuevo que presenta un gran dinamismo. Generalmente su estructura es oligopólica, dando lugar a ineficiencias asignativas, esto es, el consumidor paga un precio superior al costo marginal de provisión y por lo tanto su excedente es menor que el que obtendría en situaciones de competencia perfecta. El mercado argentino no es la excepción. El objetivo de esta investigación es sugerir, un conjunto de medidas que, sin alterar el cuadro tarifario de los prestadores, refuercen el régimen de protección de los consumidores e incrementen el nivel de satisfacción de los mismos. En particular, la investigación se focalizará en tres problemáticas que son consideradas de especial relevancia: los problemas de información, los errores en la toma de decisiones y la baja calidad de los servicios de atención al cliente. El marco teórico utilizado para el análisis es el de economía del comportamiento que, al revalorizar aspectos sociológicos y psicológicos del consumidor, dota de un mayor realismo al análisis económico convencional. Se proponen diversas medidas para cada una de las problemáticas. Para resolver los problemas de información se propone que se presente la información en formatos simplificados y en momentos críticos. En cuanto a los errores en la toma de decisiones se sugiere disminuir los costos de traspaso, eliminar las opciones predeterminadas y crear un sistema que indique a los usuarios su plan mensual óptimo. Se propone también generar incentivos para que las empresas aumenten la calidad de los servicios de atención al cliente, para lograr esto se recomienda unificar las plataformas de reclamos de las compañías y crear un sistema de recompensas para incentivas feedback de los usuarios.
+"""
+link_16018 = "https://bdigital.uncu.edu.ar/16018"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_15993 = 15993
+titulo_15993 = "El sistema de control en el Departamento de Aranceles del Hospital Dr. Humberto J. Notti, Mendoza"
+autor_15993 = "Romero, Johanna Beatriz"
+resumen_15993 = """
+El sistema de control interno puede ser aplicado para cualquier empresa indistinto de su tamaño o trayectoria y es visto más como una solución a una variedad de problemas potenciales. La presente investigación se propone indagar el problema, de las carencias de un control en los procesos del Departamento de Aranceles del Hospital Dr. Humberto J. Notti. Con el objetivo de reconocer y proponer mejoras en su sistema control. El presente estudio consiste en un análisis cuantitativo descriptivo. Como punto de partida el informe de las prácticas de trabajo en el Departamento de Aranceles del hospital, en el año 2017, realizado por la presente estudiante. Luego, se explorará la bibliografía disponible complementando con las observaciones y relevamiento de información interna, además, entrevistas realizadas a responsables de los sectores y otros colaboradores involucrados. Se evaluarán las formas de calidad del servicio, es decir eficiencia del proceso y asimismo la relación con la mejora de la motivación, comunicación y desempeño del personal del departamento del hospital público. Los resultados indican que la necesidad e importancia del control en los procesos del departamento ayuda a la organización a lograr sus metas en cuento a desempeño, rentabilidad y prevenir pérdida de recursos. Además, ayuda a asegurar que la organización cumpla con leyes y regulaciones, evitando daño a su reputación, perdida de recurso y tiempo. En fin, ayuda a llegar donde se quiere ir y evitar obstáculos a lo largo del camino.
+"""
+link_15993 = "https://bdigital.uncu.edu.ar/15993"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_16125 = 16125
+titulo_16125 = "Estrategias de operaciones aplicadas a una PyME mendocina"
+autor_16125 = "Murúa, Gabriel Leandro"
+resumen_16125 = """
+El presente trabajo tiene como objetivo demostrar como la aplicación de la herramienta distribución en planta contribuye a aumentar la eficiencia y la calidad en una organización dedicada a la producción de soda. En principio, el trabajo se enfoca en mencionar los conceptos claves de la administración de operaciones, específicamente nos basaremos en la herramienta distribución en planta. Luego nos enfocaremos en su definición, objetivos, factores, tipos y sus herramientas. Uno de los objetivos establecidos es elaborar un diagnóstico de la situación actual de la organización. Para ello se realizaron entrevistas con el personal, observaciones in situ y la elaboración de un análisis FODA. Otro de los objetivos es proponer que la organización posea una disposición flexible que sea fácilmente reajustada. A lo largo de este trabajo realizamos una descripción del proceso productivo por el cual se elabora el producto soda y un croquis que detalla la organización. Para lograr este trabajo se utilizó una metodología descriptiva en conjunto con una exploratoria que nos permite alcanzar las distintas conclusiones, entre las cuales surgieron una propuesta de la distribución de la planta y un diagrama de proceso. Por último, la investigación desarrollada demostró que con un rediseño de la distribución en planta se puede lograr mayor seguridad para los empleados y el espacio necesario para desarrollar nuevos métodos de trabajo.
+"""
+link_16125 = "https://bdigital.uncu.edu.ar/16125"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_16127 = 16127
+titulo_16127 = "Implementación de las normas ISO 9001:2015 en la Dirección General de Presupuesto de la UNCuyo"
+autor_16127 = "Ibaceta, Marcela Johana"
+resumen_16127 = """
+El presente trabajo tiene por objetivo utilizar la Norma Internacional ISO 9001:2015 como una herramienta de gestión que le permita a la organización conocer los procesos e implementar mejoras. Para tal caso, será desarrollado en la Dirección General de Presupuesto de la Universidad Nacional de Cuyo. Al inicio se introducirán los conceptos de la calidad, dado que la Norma contribuye a la implementación de un adecuado Sistema de Gestión de Calidad, basado en un enfoque de procesos. A su vez, se investigarán cuáles han sido los avances en instituciones públicas a fin de mostrar la importancia y crecimiento en el último tiempo. Las organizaciones públicas tanto como privadas buscan ser eficientes para brindar servicios de excelencia a la comunidad. Otro de los objetivos es conocer aquellos factores que afectan el desempeño del área para garantizar que la propuesta de mejora se ajuste a las verdaderas necesidades de la misma. Por lo cual, se harán entrevistas al personal, observaciones in situ y la elaboración de un análisis FODA. Para la construcción del trabajo se utilizó una metodología descriptiva en combinación con herramientas exploratorias con la finalidad de alcanzar mejores resultados. De la información relevada y análisis realizado surgió como propuesta documentar los principales procesos que se llevan a cabo en la Dirección y mejorar las líneas de comunicación. Así mismo, se comprueba la importancia que posee la registración y documentación de los procesos en cualquier organización. Las personas para mejorar su trabajo deben conocer en profundidad las tareas que realizan y responsabilizarse por los resultados de la misma. La comunicación es otro factor al cual debe prestarse atención, ya que, puede alterar la relación entre los miembros de un equipo y en consecuencia perjudicar el desempeño del mismo.
+"""
+link_16127 = "https://bdigital.uncu.edu.ar/16127"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_16129 = 16129
+titulo_16129 = "Índice de actividad económica de Mendoza, por análisis de componentes principales"
+autor_16129 = "Deluret, Fernando Javier"
+resumen_16129 = """
+El presente trabajo tiene por objetivo la construcción de un Índice de Actividad Económica (IAEMEN) para la provincia de Mendoza, el cual será de periodicidad mensual. En la actualidad no se ha publicado para la provincia ningún indicador de actividad económica de periodicidad menor a la anual. Esta investigación intenta presentar un aporte útil para satisfacer la necesidad de información actualizada de la situación económica regional. La metodología usada para la construcción del índice es el Análisis de Componentes Principales a partir de una serie de variables que se consideran representativas de la actividad económica de Mendoza, las cuales se encuentran disponibles en forma oportuna y tienen una periodicidad mensual en su publicación. El análisis de componentes principales (ACP) es un método de reducción de dimensionalidad lineal de los datos. El objetivo es poder reducir el número de variables necesarias para representar un conjunto de datos. Adicionalmente al objetivo inicial que era la construcción del índice se agregó, durante el desarrollo del trabajo, la compilación de la teoría del Análisis de Componentes Principales. Esto debido a que no existía una bibliografía en español que cubriera los diferentes aspectos del tema integrados. Como resultados del trabajo se logró construir un índice que captara los patrones de comportamiento particulares de la economía provincial, y se detectó un aparente rezago entre los movimientos de la economía de Mendoza respecto a la nacional.
+"""
+link_16129 = "https://bdigital.uncu.edu.ar/16129"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_16138 = 16138
+titulo_16138 = "La intervención del contador público en el financiamiento político"
+autor_16138 = "Flamarique, Tomás Ignacio"
+resumen_16138 = """
+Este estudio tuvo por objeto principal examinar la intervención del contador público en el financiamiento político. Y los conceptos claves que se desarrollaron fueron: partidos políticos, alianzas o frentes electorales, campaña electoral y leyes pertinentes. En cuanto a la metodología aplicada, se escogió una investigación documental mediante la cual se extrajo la información a través de libros, documentos y leyes referidas a la participación de agrupaciones políticas en el ámbito electoral, su administración, financiamiento y sanciones con respecto a los aportes recibidos. De los datos se conoce que el artículo 38 de la Constitución Nacional, define a los partidos políticos como instituciones fundamentales del sistema democrático y menciona que el Estado contribuye al sostenimiento económico de sus actividades y a la capacitación de sus dirigentes. Por su parte, los partidos políticos deben dar publicidad del origen y destino de sus fondos y patrimonio. En este punto, se destaca que su financiamiento es mixto, basados en aportes públicos y privados. Se concluye que el profesional contable, que intervenga en el financiamiento electoral debe estar al tanto de las actualizaciones de la ley y de los procedimientos a seguir, esta propuesta le proporciona los instrumentos conceptuales y prácticos para su desempeño.
+"""
+link_16138 = "https://bdigital.uncu.edu.ar/16138"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_15952 = 15952
+titulo_15952 = "Las fuentes de financiamiento de la provincia de Mendoza (2004-2016)"
+autor_15952 = "Ambrogetti, Florencia"
+resumen_15952 = """
+Las fuentes de financiamiento de la provincia de Mendoza, constantemente, son objeto de estudio de distintos organismos tanto gubernamentales como independientes. La presente investigación se propone analizar el impacto de las modificaciones del régimen de distribución de recursos nacionales en la provincia de Mendoza entre los años 2004-2016. El estudio consiste en un análisis macrosocial, diacrónico- retrospectivo y descriptivo. A partir de datos publicados por organismos oficiales, se realiza un análisis estadístico. Los datos deben ser homogeneizados para realizar correctas comparaciones entre los años mencionados. Los resultados obtenidos muestran que: la provincia de Mendoza, solo con sus recursos de origen provincial, no puede hacer frente a la totalidad de gastos, para lograrlo necesita de la transferencia de forma automática o no, por parte de la nación. En estas trasferencias se encuentran las obtenidas por la coparticipación federal, y se observa que los aportes de la provincia son mayores a lo que se obtiene por este concepto.
+"""
+link_15952 = "https://bdigital.uncu.edu.ar/15952"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_15945 = 15945
+titulo_15945 = "Ley 27349 de apoyo al capital emprendedor y la creación de la Sociedad por Acciones Simplificada (SAS)"
+autor_15945 = "Derimais, María Emilia"
+resumen_15945 = """
+El objetivo de este trabajo es informar a los estudiantes de la Universidad de Cuyo y a la comunidad interesada acerca de los principales aspectos de las Sociedades por Acciones Simplificadas, que constituye un nuevo tipo societario surgido por medio de la Ley de Apoyo al Capital Emprendedor N° 27.349, y que en forma supletoria se aplica la Ley General de Sociedades. Adicionalmente, a través de nuestra investigación, buscamos plantear los interrogantes que surgieron alrededor de este tema y realizar un análisis de la Ley 27.349, pensándola como una herramienta para los emprendedores. A través de un enfoque cualitativo, realizamos una descripción de la Ley Nº 19.550, más precisamente de aquellos artículos que se relacionan y se diferencian con los artículos de la ley que contempla a las SAS. Para poder lograr el objetivo de este trabajo, las fuentes consultadas fueron principalmente la Ley 27.349 y la Ley 19.550. Complementando a dichas leyes se analizaron las Resoluciones Generales de la Inspección General de Justicia correspondientes y de la Administración Federal de Ingresos Públicos atinentes al tema. También, nos basamos en el libro del Doctor Daniel Vitolo “Capital Emprendedor y sociedades por acciones simplificadas (SAS)". A partir del trabajo realizado y de la información obtenida, se concluye que este tipo societario, en la actualidad, es preferido por las personas al momento de invertir y constituir regularmente una empresa, esto es debido a la agilidad y facilidades dispuestas por la ley y el organismo de contralor. Cabe aclarar que en nuestra provincia no se puede hacer a través de la plataforma TAD y el trámite de constitución demora más días que en la provincia de Buenos Aires pero aún así es más conveniente que la constitución de otro tipo societario.
+"""
+link_15945 = "https://bdigital.uncu.edu.ar/15945"
+
+# -----------------------------------------------------------------------------------------------------------
+Id_15911 = 15911
+titulo_15911 = "Modelo de negocio para la creación de una tienda online"
+autor_15911 = "Tirapu Paniagua, Marina Natalia"
+resumen_15911 = """
+En el presente informe se utiliza la metodología Canvas para el desarrollo de un modelo de negocio relacionado al comercio electrónico. El propósito principal del estudio realizado es brindar a los propietarios de la empresa bajo análisis, una herramienta que permita analizar y esquematizar el posible funcionamiento de una nueva idea de negocio. Mediante el ingreso al canal de ventas digital, la administración de la empresa sostiene que sería posible expandir su operatoria y desarrollar un nuevo segmento de clientes. Como base teórica para la elaboración del informe se estudia la metodología Canvas y los conceptos y herramientas centrales relacionados con el comercio electrónico y con el marketing digital. Como primer paso antes de proceder a la construcción del modelo de negocio, se realiza un estudio situacional. La finalidad del mismo es contextualizar a la organización con respecto a su situación interna, así como en relación a su entorno externo (general y el de su industria en particular) de manera que se demuestre su capacidad para afrontar con éxito la puesta en marcha del nuevo proyecto. Finalmente se trabaja en la construcción del modelo de negocio para la creación de la tienda on line. Para esto se desarrolla en detalle cada uno de los nueve módulos que componen el lienzo del modelo de negocios de la metodología Canvas. Como resultado, se obtiene una clara visualización de los principales elementos que dan forma a la idea de negocio y que los propietarios podrán utilizar como guía para su puesta en marcha.
+"""
+link_15911 = "https://bdigital.uncu.edu.ar/15911"
+
+
+
+
+
+
+
 
 
 
@@ -63,5 +150,14 @@ tesis = [(Id_16164, titulo_16164, autor_16164, resumen_16164, link_16164),
          (Id_16159, titulo_16159, autor_16159, resumen_16159, link_16159),
          (Id_16093, titulo_16093, autor_16093, resumen_16093, link_16093),
          (Id_15912, titulo_15912, autor_15912, resumen_15912, link_15912),
-         (Id_16026, titulo_16026, autor_16026, resumen_16026, link_16026)
+         (Id_16026, titulo_16026, autor_16026, resumen_16026, link_16026),
+         (Id_16018, titulo_16018, autor_16018, resumen_16018, link_16018),
+         (Id_15993, titulo_15993, autor_15993, resumen_15993, link_15993),
+         (Id_16125, titulo_16125, autor_16125, resumen_16125, link_16125),
+         (Id_16127, titulo_16127, autor_16127, resumen_16127, link_16127),
+         (Id_16129, titulo_16129, autor_16129, resumen_16129, link_16129),
+         (Id_16138, titulo_16138, autor_16138, resumen_16138, link_16138),
+         (Id_15952, titulo_15952, autor_15952, resumen_15952, link_15952),
+         (Id_15945, titulo_15945, autor_15945, resumen_15945, link_15945),
+         (Id_15911, titulo_15911, autor_15911, resumen_15911, link_15911),
 ]
